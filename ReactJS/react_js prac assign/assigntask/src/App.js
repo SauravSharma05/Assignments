4155b717a7692a98e9b0b26b2c46c6a0b07b295f
Saurav Sharma <saurav5122000@gmail.com>
2024-06-02 21:34:27 +0530

    assg updation

--- ReactJS/react_js prac assign/assigntask/src/App.js
+++ ReactJS/react_js prac assign/assigntask/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 // import './App.css';
 import Loginpage from './Loginpage';
+import Regispage from './Regispage';
 
 
 function App() {
   return (
     <div className="App">
-      <Loginpage/>
+      {/* <Loginpage/> */}
+      <Regispage/>
       </div>
   );
 }
